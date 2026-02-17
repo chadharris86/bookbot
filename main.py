71447f920d7c53445a7b1ed240930f41c45f1559
chadharris86 <chadharris86@gmail.com>
2026-02-17 16:36:58 -0700

    004: Complete the Bookbot project.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import sys
 from stats import get_num_words
 from stats import count_characters
 from stats import counting_items
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print("============ BOOKBOT ==============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"I am reading the file at {book_path}")
     print("----------- Word Count -----------")
     print(f"Found {num_words} total words")
     print("-------- Character Count ---------")
